87c265750711cbe4ead0d3557dfe682f1019f6aa
Aidan Holland <aidanjames11@hotmail.com>
2018-08-03 17:47:56 +1000

    First commit of calculator

---
+++ src/calculator/Calculate.java
@@ -0,0 +1,44 @@
+package calculator;
+
+
+public class Calculate 
+{
+    
+    Calculate(float num1, char operator, float num2)
+    {
+        switch(operator)
+        {
+            case '+':
+                break;
+            case '-':
+                break;
+            case '*':
+                break;
+            case '/':
+                break;
+        }
+        
+    }
+    
+    float add(float num1, float num2)
+    {
+        return 0;
+    }
+    
+    float subtract(float num1, float num2)
+    {
+        return 0;
+    }
+       
+    float multiply(float num1, float num2)
+    {
+        return 0;
+    }
+          
+    float divide(float num1, float num2)
+    {
+        return 0;
+    }
+             
+             
+}
---
+++ src/calculator/Main.java
@@ -0,0 +1,12 @@
+
+package calculator;
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
